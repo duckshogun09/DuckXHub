0add044c6ddd5034dec21cf3d826513e42f310d3
duckshogun09 <vietchicoem151@gmail.com>
2025-07-15 17:58:42 +0700

    Create craftingrecipe_data.lua

---
+++ craftingrecipe_data.lua
@@ -0,0 +1,76 @@
+return {
+
+    SeedEventWorkbench = {
+        ["Horsetail"] = { ["Stonebite Seed"] = 1, ["Bamboo"] = 1, ["Corn"] = 1, ["Sheckles"] = 50000 },
+        ["Lingonberry"] = { ["Blueberry Seed"] = 3, ["Horsetail"] = 1, ["Sheckles"] = 450000 },
+        ["Amber Spine"] = { ["Cactus Seed"] = 1, ["Pumpkin"] = 1, ["Horsetail"] = 1, ["Sheckles"] = 650000 },
+        ["Grand Volcania"] = { ["Ember Lily"] = 2, ["Dinosaur Egg"] = 1, ["Ancient Seed Pack"] = 1, ["Sheckles"] = 950000 },
+        ["Peace Lily"] = { ["Rafflesia Seed"] = 1, ["Cauliflower Seed"] = 1, ["SummerCoins"] = 3 },
+        ["Aloe Vera"] = { ["Peace Lily Seed"] = 1, [" Prickly Pear"] = 1, ["SummerCoins"] = 18 },
+        ["Guanabana"] = { ["Aloe Vera Seed"] = 1, ["Prickly Pear Seed"] = 1, ["Banana"] = 1, ["SummerCoins"] = 30 },
+        ["Crafters Seed Pack"] = { ["Flower Seed Pack"] = 1, ["Honey"] = 10 },
+        ["Manuka Flower"] = { ["Daffodil Seed"] = 1, ["Orange Tulip Seed"] = 1, ["Honey"] = 6 },
+        ["Dandelion"] = { ["Manuka Flower"] = 1, ["Bamboo"] = 2, ["Honey"] = 20 },
+        ["Lumira"] = { ["Pumpkin"] = 2, ["Dandelion Seed"] = 1, ["Flower Seed Pack"] = 1, ["Honey"] = 40 },
+        ["Honeysuckle"] = { ["Pink Lily Seed"] = 1, ["Purple Dahlia Seed"] = 1, ["Honey"] = 80 },
+        ["Bee Balm"] = { ["Crocus"] = 1, ["Lavender"] = 1, ["Honey"] = 10 },
+        ["Nectar Thorn"] = { ["Cactus Seed"] = 1, ["Cactus"] = 2, ["Nectarshade Seed"] = 1, ["Honey"] = 20 },
+        ["Suncoil"] = { ["Crocus"] = 1, ["Daffodil"] = 1, ["Pink Lily"] = 1, ["Honey"] = 40 }
+    },
+
+    GearEventWorkbench = {
+        ["Lightning Rod"] = {
+            ["Basic Sprinkler"] = 1, ["Advanced Sprinkler"] = 1, ["Godly Sprinkler"] = 1, ["Sheckles"] = 500000
+        },
+        ["Reclaimer"] = {
+            ["Common Egg"] = 1, ["Harvest Tool"] = 1, ["Sheckles"] = 500000
+        },
+        ["Tropical Mist Sprinkler"] = {
+            ["Coconut"] = 1, ["Dragon Fruit"] = 1, ["Mango"] = 1, ["Godly Sprinkler"] = 1
+        },
+        ["Berry Blusher Sprinkler"] = {
+            ["Grape"] = 1, ["Blueberry"] = 1, ["Strawberry"] = 1, ["Godly Sprinkler"] = 1
+        },
+        ["Spice Spritzer Sprinkler"] = {
+            ["Pepper"] = 1, ["Ember Lily"] = 1, ["Cacao"] = 1, ["Master Sprinkler"] = 1
+        },
+        ["Flower Froster Sprinkler"] = {
+            ["Orange Tulip"] = 1, ["Daffodil"] = 1, ["Advanced Sprinkler"] = 1, ["Basic Sprinkler"] = 1
+        },
+        ["Stalk Sprout Sprinkler"] = {
+            ["Bamboo"] = 1, ["Beanstalk"] = 1, ["Mushroom"] = 1, ["Advanced Sprinkler"] = 1
+        },
+        ["Sweet Soaker Sprinkler"] = {
+            ["Watermelon"] = 3, ["Master Sprinkler"] = 1
+        },
+        ["Mutation Spray Choc"] = {
+            ["Cleaning Spray"] = 1, ["Cacao"] = 1, ["Sheckles"] = 200000
+        },
+        ["Mutation Spray Chilled"] = {
+            ["Cleaning Spray"] = 1, ["Godly Sprinkler"] = 1, ["Sheckles"] = 500000
+        },
+        ["Mutation Spray Shocked"] = {
+            ["Cleaning Spray"] = 1, ["Lightning Rod"] = 1, ["Sheckles"] = 1000000
+        },
+        ["Anti Bee Egg"] = {
+            ["Bee Egg"] = 1, ["Honey"] = 25
+        },
+        ["Small Toy"] = {
+            ["Common Egg"] = 1, ["Coconut seed"] = 1, ["Coconut"] = 1, ["Sheckles"] = 1000000
+        },
+        ["Small Treat"] = {
+            ["Common Egg"] = 1, ["Dragon Fruit seed"] = 1, ["Blueberry"] = 1, ["Sheckles"] = 1000000
+        },
+        ["Pack Bee"] = {
+            ["Anti Bee Egg"] = 1, ["Sunflower"] = 1, ["Purple Dahlia"] = 1, ["Honey"] = 250
+        },
+        ["Mutation Spray Pollinated"] = {
+            ["Cleaning Spray"] = 1, ["Bee Balm"] = 1, ["Honey"] = 25
+        },
+        ["Honey Crafters Crate"] = {
+            ["Bee Crate"] = 1, ["Honey"] = 12
+        },
+
+    },
+    VirtualCurrencies = { "Sheckles", "Honey", "SummerCoins" }
+}
